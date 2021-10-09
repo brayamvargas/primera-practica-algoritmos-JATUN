38c86946c830f2f530b78081ebcfdcfe429c52aa
brayamvargas <90940680+brayamvargas@users.noreply.github.com>
2021-10-09 17:58:09 -0400

    segunda practica

---
+++ practica2 (evaluacion de expresiones)/EJE_1.java
@@ -0,0 +1,9 @@
+
+public class EJE_1
+{ public int Operador(int valor1, int valor2, int valor3){
+    int resultado=0;
+    resultado = (valor1+valor2)/valor3;
+    return resultado;
+}
+    
+}
---
+++ practica2 (evaluacion de expresiones)/EJE_4.java
@@ -0,0 +1,9 @@
+
+public class EJE_4
+{ public int Calcular (int val1, int val2, int val3, int val4){
+    int res =0;
+    res= (val1*val2)%val3+val4;
+    return res;
+}
+
+}
