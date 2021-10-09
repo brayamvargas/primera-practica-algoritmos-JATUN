5fe3a49cfe9010963024b6a32bb3b4291a2737aa
brayamvargas <90940680+brayamvargas@users.noreply.github.com>
2021-10-09 17:55:07 -0400

    Add files via upload

---
+++ practica de lgoritmo/practica2 (evaluacion de expresiones)/EJE_2.java
@@ -0,0 +1,8 @@
+
+public class EJE_2
+{ public int Brayam (int numero1, int numero2){
+    int res =0;
+    res= numero1%numero2;
+    return res;
+}
+}
---
+++ practica de lgoritmo/practica2 (evaluacion de expresiones)/EJE_3.java
@@ -0,0 +1,13 @@
+
+public class EJE_3
+{ public String valor(int numero){
+        String res ="";
+
+        if((5!=6 || -7>10%2)&&((2+3*5)<6*5)){
+            res = "si";
+        }else{
+            res ="no";
+        }
+        return res;
+    }
+}
